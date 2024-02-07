f0eecbb7ba1fe5e91854ac74e331723443de4b83
Talha Ibne Mahmud <71333752+talhaibnmahmud@users.noreply.github.com>
2024-02-07 11:52:59 +0600

    🧼 Update: Next config for deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+	compress: false, // To handle compression in the reverse proxy (nginx)
+	images: {
+		contentDispositionType: "attachment",
+		contentSecurityPolicy:
+			"default-src 'self'; script-src 'none'; sandbox;",
+		remotePatterns: [],
+		minimumCacheTTL: 60,
+	},
+	logging: {
+		fetches: {
+			fullUrl: process.env.NODE_ENV !== "production",
+		},
+	},
 	output: undefined, // Set to "standalone" to deploy using Docker container
+	poweredByHeader: process.env.NODE_ENV !== "production",
 };
 
 export default nextConfig;
