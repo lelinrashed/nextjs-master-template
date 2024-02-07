5da1e48d7edb62b7563021ca22bac1d372ea2210
lelinrashed <lelin.rashed784@gmail.com>
2024-02-07 18:16:18 +0600

    🛠 Fix sign in and sign up page

--- app/signup/page.tsx
+++
@@ -1,9 +0,0 @@
-import { CreateAccountCard } from "@/components/create-account";
-
-export default function SignupPage() {
-	return (
-		<main className="max-w-7xl px-3 mx-auto min-h-svh grid place-content-center gap-6 md:gap-10">
-			<CreateAccountCard />
-		</main>
-	);
-}
